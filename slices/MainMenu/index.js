f08917e871b382c55d06562c3ada0893d4a0773e
Marving Moreton <marving.moreton@gmail.com>
2024-10-31 18:53:09 -0400

    adding possibility to hyperlink within description main menu

--- slices/MainMenu/index.js
+++ slices/MainMenu/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-import { PrismicRichText } from '@prismicio/react';
-import { PrismicNextImage } from '@prismicio/next';
+import { PrismicRichText } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
 
-import logoBlue from '../../public/images/logos/logo-toasteur-villeray-navy.png';
-import classes from '../../components/ui/menu/MainMenu.module.css';
+import logoBlue from "../../public/images/logos/logo-toasteur-villeray-navy.png";
+import classes from "../../components/ui/menu/MainMenu.module.css";
 
 /**
  * @typedef {import("@prismicio/client").Content.MainMenuSlice} MainMenuSlice
@@ -14,61 +14,48 @@ import classes from '../../components/ui/menu/MainMenu.module.css';
  * @param { MainMenuProps }
  */
 const MainMenu = ({ slice }) => (
-  <section className={classes['section-main-menu']}>
-    <div className={`${classes['menu-grid']} ${'container'}`}>
-      <div className={classes['box-text']}>
-        <div className={classes['left-side']}>
-          <PrismicRichText field={slice.primary.title} />
-          <span>{slice.primary.description}</span>
-        </div>
-        <div className={classes['right-side-illustration']}>
-          <PrismicNextImage
-            field={slice.primary.illustration}
-            alt={slice.primary.illustration.alt}
-          />
-        </div>
-      </div>
-      <div className={classes['menu-list']}>
-        {/* ITEM */}
+	<section className={classes["section-main-menu"]}>
+		<div className={`${classes["menu-grid"]} ${"container"}`}>
+			<div className={classes["box-text"]}>
+				<div className={classes["left-side"]}>
+					<PrismicRichText field={slice.primary.title} />
+					<span>{slice.primary.description}</span>
+				</div>
+				<div className={classes["right-side-illustration"]}>
+					<PrismicNextImage
+						field={slice.primary.illustration}
+						alt={slice.primary.illustration.alt}
+					/>
+				</div>
+			</div>
+			<div className={classes["menu-list"]}>
+				{/* ITEM */}
 
-        {slice?.items?.map((item, i) => (
-          <React.Fragment key={i}>
-            <div className={classes['item-card']}>
-              <div className={classes['title-row']}>
-                <PrismicRichText field={item.breakfasttitle} />
-                <span>{item.price}</span>
-              </div>
-              <PrismicRichText field={item.description} />
-            </div>
-          </React.Fragment>
-        ))}
-
-        {/* ITEM HIGLIGHTED */}
-        {/* Hidden as requested */}
-        {/* <div
-          className={`${classes["item-card"]} ${classes["highlighted-item"]}`}
-        >
-          <div className={classes["title-row"]}>
-            <PrismicRichText field={slice.primary.highlightedbreakfasttitle} />
-            <span>{slice.primary.highlightedbreakfastprice}</span>
-          </div>
-          <PrismicRichText
-            field={slice.primary.highlightedbreakfastdescription}
-          />
-        </div> */}
-      </div>
-    </div>
-    <div className={classes['logo-box']}>
-      <Image
-        className={classes['logo-blue-transparent']}
-        src={logoBlue}
-        alt="test"
-        // layout="fill"
-        // width={207}
-        // height={106}
-      />
-    </div>
-  </section>
+				{slice?.items?.map((item, i) => (
+					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+					<React.Fragment key={i}>
+						<div className={classes["item-card"]}>
+							<div className={classes["title-row"]}>
+								<PrismicRichText field={item.breakfasttitle} />
+								<span>{item.price}</span>
+							</div>
+							<PrismicRichText field={item.description} />
+						</div>
+					</React.Fragment>
+				))}
+			</div>
+		</div>
+		<div className={classes["logo-box"]}>
+			<Image
+				className={classes["logo-blue-transparent"]}
+				src={logoBlue}
+				alt="test"
+				// layout="fill"
+				// width={207}
+				// height={106}
+			/>
+		</div>
+	</section>
 );
 
 export default MainMenu;
